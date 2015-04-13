05e012e8dc8fa08cd72f1857b775fcd54fe7db72
Jacob Castello <jacobcastello@gmail.com>
2015-04-13 13:08:56 -0400

    initial commit after unfortunately doing most of this beforehand

---
+++ bin/app.py
@@ -0,0 +1,40 @@
+import web
+import crypt
+from web.wsgiserver import CherryPyWSGIServer
+
+CherryPyWSGIServer.ssl_certificate = "/home/jacob/pass_hasher/secret/server.crt"
+CherryPyWSGIServer.ssl_private_key = "/home/jacob/pass_hasher/secret/server.key"
+
+db = web.database(dbn="postgres", db="passhash", user="passhash", pw="urdadallday")
+t = db.transaction()
+urls = (
+        '/', 'Index'
+)
+
+app = web.application(urls, globals())
+
+render = web.template.render('templates/', base="layout")
+
+class Index(object):
+    def GET(self):
+        return render.passhash() 
+    
+    def POST(self):
+        form = web.input()
+        passw =  crypt.crypt(form.passw, '$1$/Bhh.Y3l$')
+        pass_check = crypt.crypt(form.passwcheck, '$1$/Bhh.Y3l$')
+        if  passw == pass_check:
+            content = "Passwords match."
+            try:
+                db.insert('hashes', title=passw)
+            except:
+                t.rollback()
+                raise
+            else:
+                t.commit()
+        else:
+            content = "Passwords don't match. Please press your back button and try again."
+        return render.index(content = content)
+
+if __name__ == "__main__":
+    app.run()
